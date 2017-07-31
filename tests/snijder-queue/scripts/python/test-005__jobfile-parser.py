8208e40df9520da1abf87c3900207f3c7d55e6c4
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2017-07-31 17:02:24 +0200

    Allow path to the jobfiles to be set via an environment variable.
    Otherwise, still the hardcoded variant is being used.

--- tests/snijder-queue/scripts/python/test-005__jobfile-parser.py
+++ tests/snijder-queue/scripts/python/test-005__jobfile-parser.py
@@ -14,15 +14,20 @@ except NameError:
 
 # the package is supposed to be 4 levels up in the directory hierarchy:
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + '/..' * 4))
+
 import snijder.jobs
 
 # the reload statement is here so we can use the script in subsequent calls
 # during an interactive single IPython session:
 reload(snijder.jobs)
 
+jobfile_path = 'jobfiles'
+# this allows the path to the jobfiles to be set via the environment:
+if 'SNIJDERJOBS' in os.environ:
+    jobfile_path = os.environ['SNIJDERJOBS']
 
 print '\n>>>>>> Testing CORRECT job description files:\n'
-jobfile_list = glob.glob('jobfiles/*.cfg')
+jobfile_list = glob.glob(jobfile_path + '/*.cfg')
 jobfile_list.sort()
 for jobfile in jobfile_list:
     print "----------------- parsing %s -----------------" % jobfile
@@ -32,7 +37,7 @@ for jobfile in jobfile_list:
 
 
 print '\n>>>>>> Testing INVALID job description files:'
-jobfile_list = glob.glob('jobfiles/invalid/*.cfg')
+jobfile_list = glob.glob(jobfile_path + '/invalid/*.cfg')
 jobfile_list.sort()
 for jobfile in jobfile_list:
     print "----------------- parsing %s -----------------" % jobfile
