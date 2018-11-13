42846975fedd1ae508e55be6e02cf2bc6db0f078
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2018-11-13 18:04:10 +0100

    Use colorized console output throughout the functions

--- tests/snijder-queue/functions.inc.sh
+++ tests/snijder-queue/functions.inc.sh
@@ -23,14 +23,14 @@ init() {
 
     CONFFILE="config/snijder-queue.conf"
     if ! [ -r "$CONFFILE" ] ; then
-        echo "ERROR: can't find config file, expected at '$CONFFILE'!"
+        error "Can't find config file, expected at '$CONFFILE'!"
         exit 254
     fi
     source "$CONFFILE"
 
     local QM_PY="bin/snijder-queue"
     if ! [ -f "$QM_PY" ] ; then
-        echo "ERROR: can't find queue manager executable!"
+        error "Can't find queue manager executable!"
         exit 2
     fi
 
@@ -62,10 +62,10 @@ python_can_import() {
     # which python
     # echo $PYTHONPATH
     if python -c "import $1" 2> /dev/null ; then
-        echo "Found Python package '$1'."
+        colr green "Found Python package '$1'."
         return 0
     else
-        echo "ERROR: can't import module '$1'!"
+        error "Can't import module '$1'!"
         return -1
     fi
     # set -e
@@ -83,20 +83,23 @@ check_python_packages() {
         exit 255
     fi
     if ! python_can_import "snijder" ; then
-        echo "Please adjust your PYTHONPATH accordingly, e.g."
-        echo -e "\nexport PYTHONPATH=\"$(pwd)/src:\$PYTHONPATH\"\n"
+        MSG="\nPlease adjust your PYTHONPATH accordingly, e.g.\n"
+        MSG="$MSG\nexport PYTHONPATH=\"$(pwd)/src:\$PYTHONPATH\"\n"
+        colr yellow "$MSG"
         exit 255
     fi
 }
 
 
 clean_all_spooldirs() {
+    echo -ne '\033[01;31m'  # red
     set +e
     rm -vf /data/gc3_resourcedir/shellcmd.d/*
     rm -vf "$SPOOLINGDIR/spool/cur/"*
     rm -vf "$SPOOLINGDIR/spool/new/"*
     rm -vf "$SPOOLINGDIR/queue/requests/"*
     set -e
+    echo -ne '\033[00m'  # black / white
 }
 
 
@@ -104,7 +107,7 @@ check_spooldirs_clean() {
     # test if all relevant spooling directories are empty, EXIT otherwise!
     for DIR in new cur ; do
         if ! spooldir_is_empty "$DIR" ; then
-            echo "ERROR: unclean spooling directory '$DIR' found! Stopping."
+            error "Unclean spooling directory '$DIR' found! Stopping."
             exit 1
         fi
     done
@@ -114,7 +117,7 @@ check_spooldirs_clean() {
 spooldir_is_empty() {
     # check if a given spool directory contains files
     if [ -z "$1" ] ; then
-        echo "ERROR No spooling dir specified to check!"
+        error "No spooling dir specified to check!"
         exit 255
     fi
     DIR="../../$SPOOLINGDIR/spool/$1/"
@@ -123,7 +126,7 @@ spooldir_is_empty() {
         # echo "No jobs in '$1' spooling directory!"
         return 0
     else
-        echo "WARNING: found $COUNT jobfiles in '$1': $DIR"
+        colr yellow "WARNING: found $COUNT jobfiles in '$1': $DIR"
         return 1
     fi
 }
@@ -142,9 +145,9 @@ hucore_is_running() {
 qm_request() {
     # send a status change request to the queue manager, making sure the actual
     # process is still alive (EXIT otherwise!)
-    echo "Requesting QM status change to: $1"
+    colr yellow "Requesting QM status change to: $1"
     if ! qm_is_running ; then
-        echo "ERROR: QM is not running! Stopping here."
+        error "QM is not running (any more?) - stopping here!"
         exit 3
     fi
     touch "$SPOOLINGDIR/queue/requests/$1"
@@ -172,20 +175,17 @@ startup_qm() {
     # startup_qm() could be called more than once in a single test-run, but
     # prepare_qm() should only be run if explicitly requested
     if [ -z "$QM_RUN_PREPARED" ] ; then
-        echo "ERROR: missing call to prepare_qm()."
+        error "Missing call to prepare_qm()!"
         exit 253
     fi
     if qm_is_running ; then
-        echo
-        echo "****************************************************************"
-        echo " ERROR: Queue Manager seems to be running already!"
-        echo "  --> NOT starting another one to prevent unexpected behaviour."
-        echo "****************************************************************"
-        echo
+        MSG="QM seems to be running already! \n"
+        MSG="$MSG  --> NOT starting another one to prevent unexpected behaviour."
+        error $MSG
         exit 1
     fi
-    echo "**** Starting Queue Manager..."
-    echo $QM_EXEC $QM_OPTS
+    colr green "**** Starting Queue Manager..."
+    colr green $QM_EXEC $QM_OPTS
     $QM_EXEC $QM_OPTS &
     # remember the PID of the background process:
     QM_PID=$!
@@ -196,7 +196,7 @@ startup_qm() {
     # give the QM some time to process the "refresh" request, so the followup
     # requests won't get mixed with this one:
     sleep 1
-    echo "QM process started."
+    colr green "QM process started."
 }
 
 
@@ -204,7 +204,7 @@ submit_jobs() {
     # copy jobfiles with a given prefix into the spool/new directory to submit
     # the to a running queue manager
     if [ -z "$1" ] ; then
-        echo "ERROR: no jobfile prefix given for submission!"
+        error "No jobfile prefix given for submission!"
         exit 4
     fi
     # we are expected to be in the snijder base dir, so use the full path:
@@ -220,13 +220,11 @@ wait_for_hucore_to_finish() {
     # try to terminate any still-running hucore processes
     if hucore_is_running ; then
         if [ -n "$1" ] ; then
-            echo "WARNING: Found running HuCore processes, waiting..."
+            colr yellow "WARNING: Found running HuCore processes, waiting..."
             sleep $1
         fi
         if hucore_is_running ; then
-            echo "==============================================================="
-            echo "WARNING: Found running HuCore processes, trying to kill them..."
-            echo "==============================================================="
+            colr yellow "WARNING: Found running HuCore processes, trying to kill them..."
             killall hucore.bin
         fi
     fi
@@ -238,30 +236,30 @@ wait_for_qm_to_finish() {
     # otherwise try to shut it down (gracefully, using a shutdown request), or
     # try to kill it as a last resort.
     if qm_is_running ; then
-        echo "QM is running..."
+        colr green "QM is running..."
     fi
     for counter in $(seq 1 $1) ; do
         sleep 1
         if ! qm_is_running ; then
-            echo "QM process terminated."
+            colr green "QM process terminated."
             wait_for_hucore_to_finish
             return
         fi
     done
     if qm_is_running ; then
-        echo "WARNING: QM is STILL running after $1 seconds!"
-        echo "Trying to shut it down..."
+        colr yellow "WARNING: QM is STILL running after $1 seconds!"
+        colr yellow "Trying to shut it down..."
         qm_request shutdown
         sleep 1
     fi
     if qm_is_running ; then
-        echo "WARNING: QM doesn't listen to our shutdown request!"
-        echo "Trying to kill it..."
+        colr yellow "WARNING: QM doesn't listen to our shutdown request!"
+        colr yellow "Trying to kill it..."
         pkill --signal HUP --full "$QM_EXEC"
         sleep 1
     fi
     if qm_is_running ; then
-        echo "ERROR: QM doesn't react to HUP, giving up!!"
+        error "QM doesn't react to HUP, giving up!"
     fi
     wait_for_hucore_to_finish 2
 }
@@ -275,7 +273,7 @@ queue_is_empty() {
     fi
     # the queue file *HAS TO* exist, otherwise we terminate with an error:
     if ! [ -r "$QFILE" ] ; then
-        echo "ERROR: queue file '$QFILE' doesn't exist!"
+        error "Queue file '$QFILE' doesn't exist!"
         exit 100
     fi
     # cat "$SPOOLINGDIR/queue/status/hucore.json"
@@ -292,20 +290,18 @@ queue_is_empty() {
 
 shutdown_qm_on_empty_queue() {
     if ! qm_is_running ; then
-        echo "WARNING: QM is not running!"
+        colr yellow "WARNING: QM is not running!"
         return
     fi
     for counter in $(seq 1 $1) ; do
         if queue_is_empty ; then
-            echo "Queue is empty, trying to shut down the QM!"
+            colr green "Queue is empty, trying to shut down the QM!"
             break
         fi
         sleep 1
     done
     if ! queue_is_empty ; then
-        echo "==============================================================="
-        echo "ERROR: Queue still not empty after $1 secondes!"
-        echo "==============================================================="
+        error "Queue still not empty after $1 secondes!"
     fi
     qm_request shutdown
     wait_for_qm_to_finish 5
@@ -326,8 +322,14 @@ colr() {
     local blackwhite='\033[00m'
     echo -ne "${color[$1]}"
     shift
-    echo "$*"
+    echo -e "$*"
     echo -ne "$blackwhite"
+}
+
+error() {
+    colr red "============================ ERROR ============================"
+    colr red "$*"
+    colr red "============================ ERROR ============================"
 }
 
 
