e6f08d3e0c0aa5b30d524b5af0a7be44221bf850
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2020-09-11 22:03:14 +0200

    Add a test instantiating the HuCoreApp class

--- tests/pytest/test_apps.py
+++ tests/pytest/test_apps.py
@@ -5,6 +5,7 @@
 from __future__ import print_function
 
 import snijder.apps
+import snijder.apps.hucore
 
 import pytest  # pylint: disable-msg=unused-import
 
@@ -23,3 +24,13 @@ def test_abstractapp_constructor():
     """
     with pytest.raises(TypeError, match="Refusing to instantiate class 'AbstractApp'"):
         snijder.apps.AbstractApp(job=None, appconfig=dict())
+
+
+def test_hucoreapp_constructor():
+    """Test the HuCoreApp constructor.
+
+    The constructor is expected to raise a TypeError, as this class is not supposed to
+    be instantiated itself, but rather to be inherited from.
+    """
+    with pytest.raises(TypeError, match="Not instantiating the virtual class"):
+        snijder.apps.hucore.HuCoreApp(job=None, output_dir="")
