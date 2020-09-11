3b5cbe05dcf692b3f989b7d0ea950fff071ba585
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2020-09-11 21:18:32 +0200

    Add a test instantiating the AbstractApp class

---
+++ tests/pytest/test_apps.py
@@ -0,0 +1,25 @@
+"""Tests for the snijder.apps module."""
+
+# pylint: disable-msg=invalid-name
+
+from __future__ import print_function
+
+import snijder.apps
+
+import pytest  # pylint: disable-msg=unused-import
+
+
+def prepare_logging(caplog):
+    """Helper function to set up logging appropriately."""
+    caplog.set_level("DEBUG")
+    snijder.logger.set_loglevel("debug")
+
+
+def test_abstractapp_constructor():
+    """Test the AbstractApp constructor.
+
+    The constructor is expected to raise a TypeError, as this class is not supposed to
+    be instantiated itself, but rather to be inherited from.
+    """
+    with pytest.raises(TypeError, match="Refusing to instantiate class 'AbstractApp'"):
+        snijder.apps.AbstractApp(job=None, appconfig=dict())
